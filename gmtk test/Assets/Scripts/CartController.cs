12440c3c6abd464d23db8abc9145f447ee5e65bd
pickles6969 <joshua7baker@gmail.com>
2022-07-16 10:51:15 +0100

    Cart Movement Update

--- gmtk test/Assets/Scripts/CartController.cs
+++ gmtk test/Assets/Scripts/CartController.cs
@@ -11,33 +11,66 @@ public class CartController : MonoBehaviour
 
     public float acceleration;
     public float speed;
-    private float forwardInput = 1;
+    float forwardInput = 1;
+
+    bool canMove;
+
     private float rotationSpeed = 1;
-    private Vector2 cartRot;
+    private Quaternion rotation = new Quaternion(0,0,0,1);
 
-    private Quaternion rotation = new Quaternion(0,0,0,1);  
-
-
-    // Start is called before the first frame update
     void Start()
     {
-        cartRot = transform.position;
+
     }
 
-    // Update is called once per frame
     void Update()
     {
-        //transform.position += Vector3.right * speed * Time.deltaTime;
-        if (transform.position.z > 1 || transform.position.z  < -1 )
-        {
-            Debug.Log("Trying to adjust");
-            transform.rotation = Quaternion.Lerp(transform.rotation, rotation, Time.deltaTime * rotationSpeed);
-        }
+        BalanceRotation();
     }
 
     private void FixedUpdate()
     {
+        CheckCanMove();
+        if (canMove)
+        {
+            ApplySpeed();
+        }
+        else
+        {
+            Debug.Log("Cant move");
+        }
+    }
+
+
+    void ApplySpeed()
+    {
         rearWheel.AddTorque(-forwardInput * speed * Time.deltaTime);
         frontWheel.AddTorque(-forwardInput * speed * Time.deltaTime);
+    }
+    // Adjust rotation to balance cart back to 0,0,0
+    void BalanceRotation()
+    {
+        if (transform.rotation.eulerAngles.z < -1)
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * rotationSpeed);
+        }
+        if (transform.rotation.eulerAngles.z > 1)
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * rotationSpeed);
+        }
+    }
+
+    void CheckCanMove()
+    {
+        if (transform.rotation.eulerAngles.z !< 90 && transform.rotation.eulerAngles.z !> 90)
+        {
+            canMove = true;
+            Debug.Log(canMove);
+        }
+        else
+        {
+            canMove = false;
+            Debug.Log(canMove);
+        }
     }
 }
